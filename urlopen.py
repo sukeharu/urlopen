94bdafd25132fb021c91af40ce07bf5c5fbf7a9a
Sukeharu Kano <sukeharu@solidpanda.com>
2018-12-20 13:01:56 +0900

    Update urlopen.py
    add -u, -f option. Not yet done.

--- urlopen.py
+++ urlopen.py
@@ -1,10 +1,35 @@
 #! /usr/bin/env python3
 
 import webbrowser
+import re
+from pathlib import Path
 
 
 def main():
-    webbrowser.open(args.url)
+    if args.url != '':
+        webbrowser.open(args.url)
+        return
+
+    if args.file != '':
+        if args.file.startswith('/'):
+            # absolute path
+            pass
+        else:
+            # relative path
+            path = Path(__file__)
+            filepath = path.parent.resolve() / args.file
+            url_list = find_urls(filepath)
+            print(url_list)
+
+
+def find_urls(file):
+    # Find URLs from a text file
+    # and returns list
+    text_data = open(file, 'r')
+    content = text_data.read()
+
+    pattern = re.compile('https?:\/\/[-_.!~*\'a-zA-Z0-9;\/?:\@&=+\$,%#]+')
+    return pattern.findall(content)
 
 
 if __name__ == '__main__':
@@ -13,8 +38,18 @@ if __name__ == '__main__':
         description='Open specified URL web page by a browser.'
     )
     parser.add_argument(
-        'url',
+        '-u',
+        '--url',
         help='URL to open',
+        default='',
+        type=str
+    )
+    parser.add_argument(
+        '-f',
+        '--file',
+        help='Choose text file that contains URLs. \
+        The urlopen finds URLs and open them by a browser.',
+        default='',
         type=str
     )
     args = parser.parse_args()
