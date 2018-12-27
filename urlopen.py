3af3f6e174d7e90f8f70c550680b70299002a159
Sukeharu Kano <sukeharu@solidpanda.com>
2018-12-27 11:02:16 +0900

    now a text file can be chosen.

--- urlopen.py
+++ urlopen.py
@@ -2,6 +2,8 @@
 
 import webbrowser
 import re
+import tkinter as tk
+from tkinter import filedialog
 from pathlib import Path
 
 
@@ -10,17 +12,31 @@ def main():
         webbrowser.open(args.url)
         return
 
-    if args.file != '':
-        if args.file.startswith('/'):
-            # absolute path
-            pass
-        else:
-            # relative path
-            path = Path(__file__)
-            filepath = path.parent.resolve() / args.file
+    filepath = ''
+    if args.dialog:
+        root = tk.Tk()
+        root.withdraw()
+        filepath = filedialog.askopenfilename(title='Choose Text file.')
+    else:
+        if args.file != '':
+            if args.file.startswith('/'):
+                # absolute path
+                filepath = args.file
+            else:
+                # relative path
+                path = Path(__file__)
+                filepath = path.parent.resolve() / args.file
+
+    if filepath != '':
+        try:
             url_list = find_urls(filepath)
-            for page in url_list:
-                webbrowser.open(page)
+            if len(url_list) == 0:
+                print('URL Not found in the file.')
+            else:
+                for page in url_list:
+                    webbrowser.open(page)
+        except UnicodeDecodeError as e:
+            print('Choose UTF-8 encoded text file! Error: {}'.format(e))
 
 
 def find_urls(file):
@@ -37,6 +53,14 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(
         description='Open specified URL web page by a browser.'
+    )
+    parser.add_argument(
+        '-d',
+        '--dialog',
+        help='Open file dialog. -d and -f options can\'t be specified at the same time.\
+        If both are specified, \
+        file dialog will be open (-d option will be used).',
+        action='store_true'
     )
     parser.add_argument(
         '-u',
